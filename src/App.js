1c31348be459aec93861dccca551cad5af0eb20f
Adrianne <adriannejohncamusofficial@gmail.com>
2025-03-02 22:31:24 +0800

    FIRST COMMIT FRONTEND
    ADDED A SKELETON FOR PROFILE PAGE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "../src/components/Navbar";
+import ProfilePage from "../src/pages/ProfilePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my profile - Adrianne John G. Camus
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <ProfilePage />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">Camus</h1>
+      <button className="login-btn">LOG IN</button>
+    </nav>
+  );
+};
+
+export default Navbar;
